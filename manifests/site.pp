ef26d2b3b106e66c51ab3be4ecdb262fc3de05eb
Nkster1 <36925042+Nkster1@users.noreply.github.com>
2019-03-15 13:10:13 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,10 +2,7 @@ node default {
 }
 node 'master.puppet.vm' {
   include role::master_server
-  file {'/root/README':
-    ensure => file, 
-    content => $fqdn, 
-   }
+ 
 }
 node /^web/ {
   include role::app_server
